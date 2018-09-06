ceebe294e9fc0ffac62fed4cda8f67f1eccac9d8
Soraya Sacaan M <soraya.sacaan@gmail.com>
2018-09-06 17:29:54 -0300

    update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -21,3 +21,34 @@ function resize() {
     let w = page.scrollWidth - window.innerWidth + window.innerHeight;
     document.body.style.height = w + "px";
 }
+
+// Background
+var canvas = document.getElementById("canvas");
+var altBG = document.getElementById("altBG");
+var triangle = new TriangleBG({
+    canvas : canvas,
+    alternateElem : altBG,
+    cellHeight : 120,
+    cellWidth : 100,
+    mouseLight : true,
+    mouseLightRadius : 500,
+    mouseLightIncrement : 10,
+    resizeAdjustment : true,
+    variance : 1.3,
+    pattern : "x*y",
+    baseColor1 : {
+      baseHue : 120,
+      baseSaturation : 60,
+      baseLightness : 38
+    },
+    baseColor2 : {
+      baseHue : 120,
+      baseSaturation : 60,
+      baseLightness : 40
+    },
+    colorDelta : {
+      hue : 1,
+      lightness : 0,
+      saturation : 0
+    }
+});
